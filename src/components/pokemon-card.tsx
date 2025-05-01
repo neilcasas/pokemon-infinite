7fc1a64106e2f59ed8c633aefeed3ab2e18bfb1e
Neil Casas <neilalfonzcasas@gmail.com>
2025-05-02 02:07:51 +0800

    feat: displayed type on pokemon card

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -2,16 +2,24 @@
 
 import Image from "next/image";
 import { motion } from "motion/react";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card";
+import { Badge } from "./ui/badge";
 import { Pokemon } from "@/lib/types";
-import { Card, CardContent, CardFooter, CardTitle } from "@/components/ui/card";
 
-const MotionCard = motion(Card);
+const MotionCard = motion.create(Card);
 
-export const PokemonCard = ({ id, name, image }: Pokemon) => {
+export const PokemonCard = ({ id, name, image, types }: Pokemon) => {
+  const paddedId = id.toString().padStart(3, "0");
   return (
     <MotionCard
       className="w-[320px] h-[320px] flex-shrink-0"
-      initial={{ filter: "blur(10px)", y: 100 }}
+      initial={{ filter: "blur(10px)", y: 20 }}
       whileInView={{ filter: "blur(0px)", y: 0 }}
       transition={{ duration: 0.4, ease: "easeOut" }}
       viewport={{ once: true }}
@@ -20,12 +28,21 @@ export const PokemonCard = ({ id, name, image }: Pokemon) => {
         <div className="mx-auto relative h-[240px] w-[240px]">
           <Image src={image} alt={name} fill className="object-contain" />
         </div>
-        <CardFooter className="w-full mt-2">
+        <CardFooter className="w-full mt-2 flex flex-col gap-3">
           <CardTitle className="mt-2 text-center w-full capitalize">
             {name}
           </CardTitle>
+          <CardDescription className="flex flex-col items-center justify-center">
+            <p>ID:{paddedId}</p>
+            <div className="flex gap-2 mt-2">
+              {types.map(
+                (t: { type: { name: string; url: string } }, index: number) => (
+                  <Badge key={index}>{t.type.name}</Badge>
+                ),
+              )}
+            </div>
+          </CardDescription>
         </CardFooter>
-        <input type="hidden" value={id} />
       </CardContent>
     </MotionCard>
   );
